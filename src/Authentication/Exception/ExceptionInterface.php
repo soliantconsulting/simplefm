6a6c7068e818d894703db8131ea5749e42c60ff1
Ben Scholzen <mail@dasprids.de>
2016-09-20 16:42:54 +0200

    Fix exception interfaces

--- src/Authentication/Exception/ExceptionInterface.php
+++ src/Authentication/Exception/ExceptionInterface.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace Soliant\SimpleFM\Authentication\Exception;
 
+use Throwable;
+
 interface ExceptionInterface extends Throwable
 {
 }
--- src/Client/ResultSet/Exception/ExceptionInterface.php
+++ src/Client/ResultSet/Exception/ExceptionInterface.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace Soliant\SimpleFM\Client\ResultSet\Exception;
 
+use Throwable;
+
 interface ExceptionInterface extends Throwable
 {
 }
